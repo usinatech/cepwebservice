a648607c82f9abc7a232d65cd13a7267ba0c0934
Alcimar Carlos L V e Aires <alcimar@usinahub.com>
2020-07-26 22:21:09 -0300

    Adjusts routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
     Route::get('/cepwebservice/cep/{cep}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@cep');
     Route::get('/cepwebservice/search/{q}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@search');
-    Route::get('/cepwebservice/latlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@slatlng');
-    Route::get('/cepwebservice/slatlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@sslatlng');
-    Route::get('/cepwebservice/glatlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@sglatlng');
+    Route::get('/cepwebservice/latlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@latlng');
+    Route::get('/cepwebservice/slatlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@slatlng');
+    Route::get('/cepwebservice/glatlng/{latlng}', '\UsinaHUB\CEPWebservice\CEPWebserviceController@glatlng');
 ?>
